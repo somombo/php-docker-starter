be8f812bfd838d4b0f70e4d73e73f2e47bfc059b
Chisomo Sakala <chisomo.sakala@mombo.solution>
2016-09-28 20:33:10 -0500

    initial commit

---
+++ webapp/src/index.php
@@ -0,0 +1,31 @@
+<?php
+echo "hi there<br><br>";
+
+
+
+$servername = "db";
+$username = "myusername";
+$password = "myuserpassword";
+$dbname = "mydbname";
+
+
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // sql to create table
+    $sql = "CREATE TABLE MyGuests (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL,
+    email VARCHAR(50),
+    reg_date TIMESTAMP
+    )";
+
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "Table MyGuests created successfully";
+
+
+$conn = null;
+?>
